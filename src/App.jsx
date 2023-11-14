2a81450e75e5e3a5a5433f3bbc21f1e6d2a06a93
GeovannaSCunha2 <geovanna.scunha@gmail.com>
2023-11-14 09:47:31 -0300

    Iniciando estilização da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
+import Cabecalho from './components/Cabecalho'
+import Rodape from './components/Rodape'
+import { Outlet } from 'react-router-dom'
+
+
 function App() {
+
   return (
     <>
+    <Cabecalho/>
+    <Outlet/>
+    <Rodape/>
 
     </>
   )
--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,6 +1,20 @@
-export default function Cabecalho(){
-    return (
-        <>
-        </>
-    )
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Cabecalho() {
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default Cabecalho;
