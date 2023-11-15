e580c21bf0f23bb232004fae67c8dfb06152628c
GeovannaSCunha2 <geovanna.scunha@gmail.com>
2023-11-14 21:56:52 -0300

    Cabeçalho

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,6 +1,17 @@
+import Icone from '../assets/img/icone.png'
+import '../assets/styles/rodape.scss'
+
 export default function Rodape(){
     return(
         <>
+        <div className="rodape">
+                <img className="icone" src={Icone} alt="icone" />
+            <div className='conteudoRodape'>
+                <p>&copy; 2023 | Todos os direitos reservados</p>
+                <p>Geovanna Silva Cunha - RM 97736</p>
+                <p>Mirella de Rossi imenez - RM 97990</p>
+            </div>
+        </div>
         </>
     )
 }
