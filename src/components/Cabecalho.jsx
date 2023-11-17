e71f5a4a118c1d3d32f84a51db569fca7adc738a
GeovannaSCunha2 <geovanna.scunha@gmail.com>
2023-11-17 10:45:06 -0300

    Funcionalidades finalizadas

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../assets/img/logo.png'
@@ -5,20 +6,49 @@ import '../assets/styles/cabecalho.scss'
 import { Link } from "react-router-dom";
 
 function Cabecalho() {
-  return (
-    <Navbar className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand><Link to="/"><img className="img" src={Logo} alt="logotipo" /></Link></Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <p className='textoCabecalho'>Global Solution 2023</p>
-            <p className='textoCabecalho'>Engenharia de Software</p>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("user-object");
+    sessionStorage.removeItem("token-user");
+    navigate("/");
+  };
+
+  const user = JSON.parse(sessionStorage.getItem("user-object"));
+
+  if (user) {
+    return (
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand><Link to="/"><img className="img" src={Logo} alt="logotipo" /></Link></Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <div className='infos'>
+                <span>{user.name}</span>, {user.email}
+                <button id='btnLogout' onClick={handleLogout}>Sair</button>
+              </div>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }else{
+    return (
+      <Navbar className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand><Link to="/"><img className="img" src={Logo} alt="logotipo" /></Link></Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <p className='textoCabecalho'>Global Solution 2023</p>
+              <p className='textoCabecalho'>Engenharia de Software</p>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
 
 export default Cabecalho;
--- src/routes/Cadastro.jsx
+++ src/routes/Cadastro.jsx
@@ -1,11 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import '../assets/styles/formulario.scss'
-import img1 from '../assets/img/img1.jpg'
+import '../assets/styles/formulario.scss';
+import img1 from '../assets/img/img1.jpg';
 
 export default function Cadastra() {
   const [email, setEmail] = useState('');
-  const [fullName, setFullName] = useState('');
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
@@ -14,21 +15,21 @@ export default function Cadastra() {
   const handleSubmit = async (event) => {
     try {
       event.preventDefault();
-  
+
       if (password !== confirmPassword) {
         throw new Error('As senhas não correspondem.');
       }
-  
+
       const response = await fetch('http://localhost:5000/users', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email, fullName, username, password }),
+        body: JSON.stringify({ email, name, lastName, username, password }),
       });
-  
+
       const data = await response.json();
-  
+
       if (data.id) {
         alert('Usuário registrado com sucesso!');
         navigate('/');
@@ -43,38 +44,42 @@ export default function Cadastra() {
 
   return (
     <>
-        <div className='cadastro'>
-            <div className='conteudoCadastro'>
-                <h1 className='titulo'>Cadastre-se!</h1>
-                <p id='parag'>Com a HealthPartner você terá controle e a avaliação</p>
-                <p id='parag2'>de seus exames de forma prática e simplificada.</p>
+      <div className='cadastro'>
+        <div className='conteudoCadastro'>
+          <h1 className='titulo'>Cadastre-se!</h1>
+          <p id='parag'>Com a HealthPartner você terá controle e a avaliação</p>
+          <p id='parag2'>de seus exames de forma prática e simplificada.</p>
 
-                <img src={img1} id='img1' alt="img ilustrativa" width="80%" />
-            </div>
-            <form className="bg-body-tertiary" id='formulario' onSubmit={handleSubmit}>
-            <label>
-                E-mail:
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            </label>
-            <label>
-                Nome completo:
-                <input className="input"type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} required />
-            </label>
-            <label>
-                Nome de usuário:
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
-            </label>
-            <label>
-                Senha:
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-            </label>
-            <label>
-                Confirmar senha:
-                <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
-            </label>
-            <button type="submit">Registrar</button>
-            </form> 
+          <img src={img1} id='img1' alt="img ilustrativa" width="80%" />
         </div>
+        <form className="bg-body-tertiary" id='formulario' onSubmit={handleSubmit}>
+          <label>
+            E-mail:
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          </label>
+          <label>
+            Primeiro nome:
+            <input className="input" type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+          </label>
+          <label>
+            Sobrenome:
+            <input className="input" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+          </label>
+          <label>
+            Nome de usuário:
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+          </label>
+          <label>
+            Senha:
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          </label>
+          <label>
+            Confirmar senha:
+            <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+          </label>
+          <button type="submit">Registrar</button>
+        </form> 
+      </div>
     </>
   );
 }
